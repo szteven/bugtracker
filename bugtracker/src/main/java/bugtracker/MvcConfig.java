7c0215fddfded487ddac90ad9cdd4d946ef8a29e
Istvan Szollosi <szollosi@gmail.com>
2019-11-30 01:51:43 +0100

    Set mvc config for index.html and main.html

--- bugtracker/src/main/java/bugtracker/MvcConfig.java
+++ bugtracker/src/main/java/bugtracker/MvcConfig.java
@@ -12,6 +12,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/").setViewName("login");
+		registry.addViewController("/main").setViewName("main");
+		registry.addViewController("/index").setViewName("index");
 		registry.addViewController("/home").setViewName("hello");
 		registry.addViewController("/login").setViewName("login");
 		registry.addViewController("/ticket/all").setViewName("tickets");
